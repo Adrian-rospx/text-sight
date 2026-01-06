6f0731a308364cbca690db22667ab3dc26af9311
Adrian-rospx <adrianrosu5400@gmail.com>
2026-01-06 22:52:05 +0200

    init: Dependencies installed and configured

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+
+#include <iostream>
+
+int main() {
+    std::cout << "Program dependencies check\n";
+    
+}
