0eae9bc522049f5ef0e17b98f6d49f4aec6455d5
Adrian-rospx <adrianrosu5400@gmail.com>
2026-01-07 00:08:53 +0200

    first UI test

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/core.hpp>
+#include <opencv2/core/hal/interface.h>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
@@ -8,6 +9,26 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Program dependencies check\n";
+    cv::Mat image(cv::Mat::zeros(200, 200, CV_8UC3));
+    cv::circle(image, {100, 100}, 30, {0, 0, 255}, -1);
+
+    using namespace ftxui;
+    // Create a simple document with three text elements.
+    Element document = hbox({
+        text("left")   | border,
+        text("middle") | border | flex,
+        text("right")  | border,
+    });
     
+    // Create a screen with full width and height fitting the document.
+    auto screen = Screen::Create(
+        Dimension::Full(),       // Width
+        Dimension::Fit(document) // Height
+    );
+    
+    // Render the document onto the screen.
+    Render(screen, document);
+    
+    // Print the screen to the console.
+    screen.Print();
 }
