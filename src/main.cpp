08795452453a7c07c59918eeb1c45a739cea8ebe
Adrian-rospx <adrianrosu5400@gmail.com>
2026-01-07 17:38:23 +0200

    added: camera input

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,37 @@
 #include <opencv2/core/hal/interface.h>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
 
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 
 #include <iostream>
 
-int main() {
-    cv::Mat image(cv::Mat::zeros(200, 200, CV_8UC3));
-    cv::circle(image, {100, 100}, 30, {0, 0, 255}, -1);
+int main(int argc, char* argv[]) {
+    // attempt opening a video camera
+    cv::VideoCapture camera(0);
+    if (!camera.isOpened()) {
+        std::cerr << "ERROR: Could not open camera\n";
+        return 1;
+    }
 
-    cv::imshow("Test", image);
-    cv::waitKey(1000);
+    // open webcam window
+    cv::namedWindow("Image input", cv::WINDOW_NORMAL);
+
+    cv::Mat frame;
+
+    // main loop
+    while (true) {
+        // capture frame
+        camera >> frame;
+
+        // display
+        cv::imshow("Image input", frame);
+
+        // wait 10ms; break on Esc
+        if (cv::waitKey(10) == 27) {
+            break;
+        }
+    }
 }
