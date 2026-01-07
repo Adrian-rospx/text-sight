eeaa9af6b6ce9fb28c76d1d22621e392e680de78
Adrian-rospx <adrianrosu5400@gmail.com>
2026-01-07 19:37:52 +0200

    update: ascii image spammer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,46 @@
+#include <ftxui/dom/node.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/core/hal/interface.h>
+#include <opencv2/core/types.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 
+#include <ftxui/screen/screen.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+using namespace ftxui;
+
+const std::string_view textGradient { " .,-;<+*/ixaA8#M" };
+
+std::string imageToASCII(const cv::Mat& frame, const Screen& screen) {
+    const int width { screen.dimx() };
+    const int height { screen.dimy() };
+
+    cv::Mat resized;
+    cv::resize(frame, resized, cv::Size(width, height));
+    cv::cvtColor(resized, resized, cv::COLOR_BGR2GRAY);
+
+    std::string str;
+    str.reserve((width + 1) * height);
+
+    for (int y{}; y < resized.rows; ++y) {
+        for (int x{}; x < resized.cols; ++x) {
+            const std::uint8_t pixel { resized.at<std::uint8_t>(y, x) };
+
+            const int index = pixel * (textGradient.size() - 1) / 255;
+            str += textGradient[index];
+        }
+        str += '\n';
+    }
+
+    return str;
+}
 
 int main(int argc, char* argv[]) {
     // attempt opening a video camera
@@ -19,7 +52,8 @@ int main(int argc, char* argv[]) {
 
     // open webcam window
     cv::namedWindow("Image input", cv::WINDOW_NORMAL);
-
+    auto termScreen { ScreenInteractive::Create(Dimension::Full()) };
+    
     cv::Mat frame;
 
     // main loop
@@ -29,6 +63,13 @@ int main(int argc, char* argv[]) {
 
         // display
         cv::imshow("Image input", frame);
+        
+        const std::string asciiText { imageToASCII(frame, termScreen) };
+        const auto asciiScreen { paragraph(asciiText ) };
+
+        termScreen.Clear();
+        Render(termScreen, asciiScreen);
+        termScreen.Print();
 
         // wait 10ms; break on Esc
         if (cv::waitKey(10) == 27) {
