daf4405153cd740bb82aa223d1762f2aac871a80
Adrian-rospx <adrianrosu5400@gmail.com>
2026-01-08 15:20:51 +0200

    update: modularized, componentized

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,32 @@
 #include <opencv2/core.hpp>
-#include <opencv2/core/hal/interface.h>
+#include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/event.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/node.hpp>
-#include <ftxui/screen/screen.hpp>
 #include <ftxui/dom/elements.hpp>
 
-#include <chrono>
+#include <cstdint>
 #include <iostream>
 #include <string>
-#include <cstdint>
 #include <thread>
 
 using namespace ftxui;
 
 const std::string_view textGradient { " .,-;<+*/ixaA8#M" };
 
-std::string imageToASCII(const cv::Mat& frame, const Screen& screen) {
-    const int width { screen.dimx() };
-    const int height { screen.dimy() };
+struct AppState {
+    std::string ascii_frame{};
+    bool running { true };
+    int width { 120 };
+    int height { 40 };
+};
 
+std::string imageToASCII(const cv::Mat& frame, const int width, const int height) {
     cv::Mat resized;
     cv::resize(frame, resized, cv::Size(width, height));
     cv::cvtColor(resized, resized, cv::COLOR_BGR2GRAY);
@@ -46,59 +47,62 @@ std::string imageToASCII(const cv::Mat& frame, const Screen& screen) {
     return str;
 }
 
+void camera_loop(AppState& state, cv::VideoCapture& camera, ScreenInteractive& screen) {
+    using namespace std::chrono_literals;
+
+    cv::Mat frame;
+
+    while (state.running) {
+        camera >> frame;
+        if (frame.empty()) continue;
+
+        state.width = screen.dimx();
+        state.height = screen.dimy();
+
+        state.ascii_frame = imageToASCII(frame, state.width, state.height);
+
+        screen.PostEvent(Event::Custom);
+
+        std::this_thread::sleep_for(33ms); 
+    }
+}
+
+Component MakeView(AppState& state) {
+    return Renderer([&]() {
+        return paragraph(state.ascii_frame);
+    });
+}
+
+Component MakeController(AppState& state, ScreenInteractive& screen,Component view) {
+    return CatchEvent(view, [&](Event event) {
+        if (event == Event::Character('q')) {
+            state.running = false;
+            screen.Exit();
+            return true;
+        }
+        return false;
+    });
+}
+
 int main(int argc, char* argv[]) {
-    // attempt opening a video camera
+    AppState state {};
+
     cv::VideoCapture camera(0);
     if (!camera.isOpened()) {
         std::cerr << "ERROR: Could not open camera\n";
         return 1;
     }
 
-    // open webcam window
-    // cv::namedWindow("Image input", cv::WINDOW_NORMAL);
-    auto termScreen { ScreenInteractive::Fullscreen() };
-    
-    std::string asciiText;
-    
-    bool running {true};
+    // start terminal screen
+    auto screen { ScreenInteractive::Fullscreen() };
 
-    std::thread worker([&] {
-        cv::Mat frame;
-        
-        // main loop
-        while (running) {
-            // capture frame
-            camera >> frame;
-            if (frame.empty()) continue;
+    auto view = MakeView(state);
+    auto app  = MakeController(state, screen, view);
 
-            // display
-            // cv::imshow("Image input", frame);
+    std::thread worker(camera_loop, std::ref(state), std::ref(camera), std::ref(screen));
 
-            asciiText = imageToASCII(frame, termScreen);
-            termScreen.PostEvent(Event::Custom);
-        
-            std::this_thread::sleep_for(std::chrono::milliseconds(30));
-            // if (cv::waitKey(30) == 'q') {
-            //     break;
-            // }
-        }
-    });
+    screen.Loop(app);
 
-    auto renderer = Renderer([&]() {
-        return paragraph(asciiText);
-    });  
-
-    auto application = CatchEvent(renderer, [&](Event event) {
-        if (event == Event::Character('q')) {
-            running = false;
-            termScreen.Exit();
-            return true;
-        } 
-        return false;
-    });
-
-    termScreen.Loop(application);
-
-    running = false;
+    state.running = false;
     worker.join();
 }
